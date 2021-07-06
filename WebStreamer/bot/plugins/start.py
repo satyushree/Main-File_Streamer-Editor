75df651ddcb8604a46977def8df1a904d012ec23
Shreenidhi <66470817+satyushree@users.noreply.github.com>
2021-07-06 11:25:23 +0530

    Update start.py

--- WebStreamer/bot/plugins/start.py
+++ WebStreamer/bot/plugins/start.py
@@ -101,12 +101,7 @@ async def start(b, m):
                     parse_mode="HTML",
                     disable_web_page_preview=True)
                 return
-        await m.reply_text(
-            text=START_TEXT.format(m.from_user.mention),
-            parse_mode="HTML",
-            disable_web_page_preview=True,
-            reply_markup=START_BUTTONS
-              )                                                                         
+        
                                                                                        
                                                                             
     else:
